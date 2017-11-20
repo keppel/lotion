caf5ad13b498cd9398505ffe68c2b54ec9c3cb6d
Judd <kepzorz@gmail.com>
2017-11-20 04:31:19 -0500

    download basecli on osx

--- bin/binaries.js
+++ bin/binaries.js
@@ -1,5 +1,9 @@
-module.exports = {
+exports.tendermint = {
   darwin: 'https://github.com/tendermint/tendermint/releases/download/v0.11.0/tendermint_0.11.0_darwin_amd64.zip',
   win32: 'https://github.com/tendermint/tendermint/releases/download/v0.11.0/tendermint_0.11.0_windows_amd64.zip',
   linux: 'https://github.com/tendermint/tendermint/releases/download/v0.11.0/tendermint_0.11.0_linux_amd64.zip'
+}
+
+exports.basecli = {
+  darwin: 'https://tendermint.nyc3.digitaloceanspaces.com/basecli-darwin-v0.7.0'
 }
