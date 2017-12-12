a69b3b15b1ba16fca86c8d1260c88837c5fa502a
Judd <kepzorz@gmail.com>
2017-12-12 18:48:07 -0500

    update binary downloads

--- bin/binaries.js
+++ bin/binaries.js
@@ -1,9 +1,5 @@
 exports.tendermint = {
-  darwin: 'https://github.com/tendermint/tendermint/releases/download/v0.11.0/tendermint_0.11.0_darwin_amd64.zip',
-  win32: 'https://github.com/tendermint/tendermint/releases/download/v0.11.0/tendermint_0.11.0_windows_amd64.zip',
-  linux: 'https://github.com/tendermint/tendermint/releases/download/v0.11.0/tendermint_0.11.0_linux_amd64.zip'
-}
-
-exports.basecli = {
-  darwin: 'https://tendermint.nyc3.digitaloceanspaces.com/basecli-darwin-v0.7.0'
+  darwin: 'https://github.com/tendermint/tendermint/releases/download/v0.14.0/tendermint_0.14.0_darwin_amd64.zip',
+  win32: 'https://github.com/tendermint/tendermint/releases/download/v0.14.0/tendermint_0.14.0_windows_amd64.zip',
+  linux: 'https://github.com/tendermint/tendermint/releases/download/v0.14.0/tendermint_0.14.0_linux_amd64.zip'
 }
