5be40b99914f299a54677cc4f3cc861768a6d3aa
Judd <kepzorz@gmail.com>
2018-01-16 20:32:10 -0800

    update to tendermint v0.15.0

--- bin/binaries.js
+++ bin/binaries.js
@@ -1,5 +1,5 @@
 exports.tendermint = {
-  darwin: 'https://tendermint.nyc3.digitaloceanspaces.com/darwin-tendermint-v0.14.0.zip',
-  win32: 'https://tendermint.nyc3.digitaloceanspaces.com/windows-tendermint-v0.14.0.zip',
-  linux: 'https://tendermint.nyc3.digitaloceanspaces.com/linux-tendermint-v0.14.0.zip'
+  darwin: 'https://tendermint.nyc3.digitaloceanspaces.com/darwin-tendermint-v0.15.0.zip',
+  win32: 'https://tendermint.nyc3.digitaloceanspaces.com/windows-tendermint-v0.15.0.zip',
+  linux: 'https://tendermint.nyc3.digitaloceanspaces.com/linux-tendermint-v0.15.0.zip'
 }
