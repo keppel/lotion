6e21a0e547d850583901af2867d39aa48c5d393e
Judd <kepzorz@gmail.com>
2018-02-08 19:49:44 -0800

    simplify transaction server endpoints

--- example/counter/lite.js
+++ example/counter/lite.js
@@ -1,12 +1,9 @@
 let { connect } = require('../../')
 
 async function main() {
-  let { getState, send } = await connect(process.env.GCI)
-  console.log(await getState('foo.bar'))
+  let { send, state } = await connect(process.env.GCI)
+  console.log(await state.foo.bar)
+  console.log(await state)
 }
-
-process.on('unhandledRejection', e => {
-  console.log(e)
-})
 
 main()
