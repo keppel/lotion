41b9de4f4005ffbc0e0461b24213efdace76ec67
MisterDr <djenadrazic@gmail.com>
2018-04-16 21:36:38 +0200

    - Added first release to support version v0.19.0

--- bin/binaries.js
+++ bin/binaries.js
@@ -1,5 +1,5 @@
 exports.tendermint = {
-  darwin: 'https://tendermint.nyc3.digitaloceanspaces.com/darwin-tendermint-v0.15.0.zip',
+  darwin: 'https://github.com/AltCoinExchange/altcoin-node/releases/download/0.19.0/darwin-tendermint-v0.19.0.zip',
   win32: 'https://tendermint.nyc3.digitaloceanspaces.com/windows-tendermint-v0.15.0.zip',
-  linux: 'https://tendermint.nyc3.digitaloceanspaces.com/linux-tendermint-v0.15.0.zip'
+  linux: 'https://github.com/AltCoinExchange/altcoin-node/releases/download/0.19.0/linux-tendermint-v0.19.0.zip'
 }
