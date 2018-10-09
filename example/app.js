7df8729f02d1f98d1f4ef8ec82df71da7751bf6b
Arne Meeuw <arne.meeuw@gmail.com>
2018-10-09 12:49:13 +0200

    Update local example

--- example/app.js
+++ example/app.js
@@ -1,9 +1,20 @@
 let lotion = require('../dist/index')
 
+console.log(`Keys: ${process.argv[2]}`)
+console.log(`Genesis: ${process.argv[3]}`)
+console.log(`Peer: ${process.argv[4]}`)
+console.log(`RPC: ${process.argv[5]}`)
+console.log(`P2P: ${process.argv[6]}`)
+console.log(`Lotion: ${process.argv[7]}`)
+
 let app = lotion({
+  keyPath: process.argv[2],
+  genesisPath: process.argv[3],
+  peers: [process.argv[4]],
+  rpcPort: process.argv[5],
+  p2pPort: process.argv[6],
+  lotionPort: process.argv[7],
   logTendermint: false,
-  emptyBlocksInterval: 12,
-  devMode: false,
   initialState: {
     count: 0
   }
@@ -18,7 +29,7 @@ app.use(function(state, tx) {
 
 
 app.useBlock(function(state, chainInfo) {
-  console.log(chainInfo)
+  console.log(`Blocktime: ${chainInfo.time}`)
 })
 
 app.start().then(console.log)
